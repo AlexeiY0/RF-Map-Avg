491b5eb66a38d5600c8842f655b27df309c338a8
AlexeiY0 <147881231+AlexeiY0@users.noreply.github.com>
2023-10-21 12:21:39 +1300

    Add files via upload

---
+++ data/1001000_13.js
@@ -0,0 +1,18 @@
+var json_1001000_13 = {
+"type": "FeatureCollection",
+"name": "1001000_13",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 226.248, "PD_AVG_(mW/m2)": 109.589, "PD_MIN_(mW/m2)": 28.943, "Source": "Mixed", "Number_of_points": "4" }, "geometry": { "type": "Point", "coordinates": [ 174.909422128743216, -36.889869192510631 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 303.162, "PD_AVG_(mW/m2)": 151.604, "PD_MIN_(mW/m2)": 0.045, "Source": "Cell Tower", "Number_of_points": "2" }, "geometry": { "type": "Point", "coordinates": [ 174.885104220000244, -36.90712896000003 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 303.162, "PD_AVG_(mW/m2)": 101.129, "PD_MIN_(mW/m2)": 0.086, "Source": "Mixed", "Number_of_points": "3" }, "geometry": { "type": "Point", "coordinates": [ 174.885064346671356, -36.907261918333802 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 155.133, "PD_AVG_(mW/m2)": 155.133, "PD_MIN_(mW/m2)": 155.133, "Source": "Cell Tower", "Number_of_points": "1" }, "geometry": { "type": "Point", "coordinates": [ 174.929724959999987, -36.892755485 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 4793.394, "PD_AVG_(mW/m2)": 808.376, "PD_MIN_(mW/m2)": 4.183, "Source": "Cell Tower", "Number_of_points": "6" }, "geometry": { "type": "Point", "coordinates": [ 174.911049743332455, -36.8816745908345 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 384.904, "PD_AVG_(mW/m2)": 203.495, "PD_MIN_(mW/m2)": 79.383, "Source": "Mixed", "Number_of_points": "4" }, "geometry": { "type": "Point", "coordinates": [ 174.909137412497842, -36.889816170005446 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 176.832, "PD_AVG_(mW/m2)": 103.813, "PD_MIN_(mW/m2)": 35.36, "Source": "WiFi/Phone", "Number_of_points": "3" }, "geometry": { "type": "Point", "coordinates": [ 174.908678133329602, -36.889823247782097 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 4793.394, "PD_AVG_(mW/m2)": 218.799, "PD_MIN_(mW/m2)": 7.751, "Source": "Cell Tower", "Number_of_points": "23" }, "geometry": { "type": "Point", "coordinates": [ 174.856775530435783, -36.902118604565942 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 289.473, "PD_AVG_(mW/m2)": 174.555, "PD_MIN_(mW/m2)": 83.78, "Source": "Mixed", "Number_of_points": "7" }, "geometry": { "type": "Point", "coordinates": [ 174.909007349999229, -36.889801028574375 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 167.552, "PD_AVG_(mW/m2)": 116.539, "PD_MIN_(mW/m2)": 37.319, "Source": "Mixed", "Number_of_points": "3" }, "geometry": { "type": "Point", "coordinates": [ 174.909257454999079, -36.88983542166924 ] } },
+{ "type": "Feature", "properties": { "PD_MAX_(mW/m2)": 329.962, "PD_AVG_(mW/m2)": 218.167, "PD_MIN_(mW/m2)": 106.371, "Source": "WiFi/Phone", "Number_of_points": "2" }, "geometry": { "type": "Point", "coordinates": [ 174.909240517481862, -36.890046657519534 ] } }
+]
+}
